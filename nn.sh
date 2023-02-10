07ed9c0dca4e6aa3939b713e23ed17341c8b43b6
Rikard Karlsen <rk@cassettian.space>
2023-02-10 08:58:06 +0100

    feat(nn.sh): make it pretty

--- nn.sh
+++ nn.sh
@@ -6,8 +6,12 @@ while : ; do
 	file="$(find "$dir" -type f -not -path "$dir/.git/*" -printf "%T@ %P\n" \
 		| sort -nr \
 		| cut -d" " -f2- \
-		| fzf --preview-window=up --preview "glow --style dark {}")"
+		| sed -e 's|/| » |g' \
+		| fzf --preview-window=up \
+			--preview='echo "{}" | sed -e "s| » |/|g" | xargs glow --style dark')"
+
 	[ "$?" = 130 ] && break
-	[ -n "$file" ] && nvim "$file"
+	file="$(echo $file | sed -e 's| » |/|g')"
+	[ -n "$file" ] && nvim -c "ZenMode" "$file"
 done
 
